3188560e39ec69b313513a835480eebe004f47b0
Alex Mandelias <alexmandelias@gmail.com>
2024-07-17 17:37:35 +0300

    Adds TimeControl data class and Serializer
    TimeControl stores information relevant to the allotted time for each
    player, the increment after each turn, and the type thereof. To conform
    to the Identifiable interface, the appropriate properties are added.
    
    To serialize a TimeControl, just write all of its fields to a string in
    a dash-separated manner, and deserialize similarly.
    
    The serializer and the implementation of the Identifiable interface are
    the only changes needed to be able to store TimeControl data using our
    repository system.

---
+++ app/src/main/java/alexman/chessclock_multiplayer/model/TimeControl.kt
@@ -0,0 +1,35 @@
+package alexman.chessclock_multiplayer.model
+
+import alexman.chessclock_multiplayer.repository.Identifiable
+
+// TODO: document
+data class TimeControl(
+    // should be ID_NOT_SET when creating new TimeControl
+    override var id: Int = ID_NOT_SET,
+    val timeSeconds: Int,
+    val incrementSeconds: Int,
+    val type: TimeControlType,
+) : Identifiable {
+
+    override val id_not_set_constant: Int = ID_NOT_SET
+
+    companion object {
+        private const val ID_NOT_SET = -1
+
+        // use this instead of constructor, it sets id automatically
+        fun new(
+            timeSeconds: Int,
+            incrementSeconds: Int,
+            type: TimeControlType,
+        ) = TimeControl(
+                ID_NOT_SET, timeSeconds, incrementSeconds, type
+            )
+    }
+}
+
+// TODO: document
+enum class TimeControlType {
+    FISHER,
+    BRONSTEIN,
+    DELAY,
+}
--- app/src/main/java/alexman/chessclock_multiplayer/repository/Serializer.kt
+++ app/src/main/java/alexman/chessclock_multiplayer/repository/Serializer.kt
@@ -1,14 +1,39 @@
 package alexman.chessclock_multiplayer.repository
 
+import alexman.chessclock_multiplayer.model.TimeControl
+import alexman.chessclock_multiplayer.model.TimeControlType
+
 // TODO: document
 interface Serializer<T, U> {
 
     fun serialize(item: T): U
 
-    fun deserialize(serialized: U): T
+    fun deserialize(serializedItem: U): T
 
     companion object
 }
 
 // TODO: document
 interface StringSerializer<T>: Serializer<T, String>
+
+// TODO: document
+val Serializer.Companion.StringTimeControlSerializer
+    get() = object : StringSerializer<TimeControl> {
+
+        override fun serialize(item: TimeControl): String =
+            with(item) {
+                "$id-$timeSeconds-$incrementSeconds-$type"
+            }
+
+        private val regex = "(\\d+)-(\\d+)-(\\d+)-(\\w+)"
+
+        override fun deserialize(serializedItem: String): TimeControl =
+            Regex(regex).matchEntire(serializedItem)!!.let {
+                TimeControl(
+                    it.groupValues[1].toInt(),
+                    it.groupValues[2].toInt(),
+                    it.groupValues[3].toInt(),
+                    TimeControlType.valueOf(it.groupValues[4]),
+                )
+            }
+    }
