5d1cd4fc12542956a919efdcfc777adfa9854d52
Alex Mandelias <alexmandelias@gmail.com>
2024-07-24 17:16:00 +0300

    Adds EditTimeControl screen
    This screen allows users to edit TimeControl data. Time and Increment
    can be editted using plain text, and are decoded using a rather
    permissive set of regular expressions. The type of increment is
    determiend by a collection of radio buttons.
    
    Changelist:
    - Adds EditTimeControlScreen composable
    - Adds Parser to TimeControl, which handles parsing, formatting and
      validating time-related data
    - Adds new test data and Screen enum constant for this screen in
      MainActivity

--- app/src/main/java/alexman/chessclock_multiplayer/MainActivity.kt
+++ app/src/main/java/alexman/chessclock_multiplayer/MainActivity.kt
@@ -9,6 +9,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import alexman.chessclock_multiplayer.designsystem.theme.ChckmTheme
+import alexman.chessclock_multiplayer.model.TimeControl
+import alexman.chessclock_multiplayer.model.TimeControlType
+import alexman.chessclock_multiplayer.ui.EditTimeControlScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,17 +26,28 @@ class MainActivity : ComponentActivity() {
 }
 
 private enum class Screen {
+    EDIT_TIMECONTROL,
     NONE,
 }
 
 @Composable
 private fun TestScreen() {
 
-    val screen = Screen.NONE
+    val screen = Screen.EDIT_TIMECONTROL
+
+    val timeControlTestData = listOf(
+        TimeControl.load(0, 180, 1, TimeControlType.FISHER),
+        TimeControl.load(1, 60, 1, TimeControlType.BRONSTEIN),
+        TimeControl.load(2, 300, 5, TimeControlType.DELAY),
+    )
 
     Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
         when (screen) {
             Screen.NONE -> { }
+            Screen.EDIT_TIMECONTROL -> EditTimeControlScreen(
+                timeControl = timeControlTestData[0],
+                onSubmitTimeControl = { println("TimeControl data: $it") }
+            )
         }
     }
 }
--- app/src/main/java/alexman/chessclock_multiplayer/model/TimeControl.kt
+++ app/src/main/java/alexman/chessclock_multiplayer/model/TimeControl.kt
@@ -29,5 +29,56 @@ data class TimeControl private constructor( // use new() and load() instead of c
 
         fun load(id: Int, timeSeconds: Int, incrementSeconds: Int, type: TimeControlType) =
             TimeControl(id, timeSeconds, incrementSeconds, type)
+
+        object Parser {
+
+            private val regexMin = Regex( // 3 min
+                pattern = "(\\d+)\\s*(?:m|min|mins|minute|minutes)"
+            )
+            private val regexSec = Regex( // 1 sec
+                pattern = "(\\d+)\\s*(?:s|sec|secs|second|seconds)"
+            )
+            private val regexMinSec = Regex( // 3 min, 1 sec
+                pattern = "${regexMin.pattern}[\\s,/|+\\-]*?${regexSec.pattern}"
+            )
+
+            fun validate(time: String): Boolean =
+                time.trim().let {
+                    return@let regexMin.matches(it) || regexSec.matches(it)
+                            || regexMinSec.matches(it)
+                }
+
+            fun parse(time: String): Int {
+                if (!validate(time))
+                    throw IllegalArgumentException(
+                        "\"$time\" does not correspond to a valid time string"
+                    )
+
+                val trimmedTime = time.trim()
+                regexMin.matchEntire(trimmedTime)?.groupValues?.let { return it[1].toInt() * 60 }
+                regexSec.matchEntire(trimmedTime)?.groupValues?.let { return it[1].toInt() }
+                regexMinSec.matchEntire(trimmedTime)?.groupValues?.let {
+                    return it[1].toInt() * 60 + it[2].toInt()
+                }
+
+                // one of the above ?.let calls is guaranteed to return
+                // because of `if` statement above, but kotlin doesn't know it
+                throw Error("Why are we here? Just to satisfy the compiler?")
+            }
+
+            fun format(seconds: Int): String {
+                val min = "min"
+                val sec = "sec"
+
+                if (seconds == 0)
+                    return "0 $sec"
+
+                val m: String = if (seconds > 59) "${seconds / 60} $min" else ""
+                val s: String = if (seconds % 60 != 0) "${seconds % 60} $sec" else ""
+
+                return if (listOf(m, s).all { it != "" })
+                    "$m $s" else "$m$s"
+            }
+        }
     }
 }
