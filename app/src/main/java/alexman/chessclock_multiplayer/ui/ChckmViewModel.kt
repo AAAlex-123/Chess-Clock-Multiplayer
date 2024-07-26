a1700bf6bc16a319b442438f9026d46875343fae
Alex Mandelias <alexmandelias@gmail.com>
2024-07-27 01:46:13 +0300

    Adds ChckmViewModel
    The view model which will be used throughout this application.

---
+++ app/src/main/java/alexman/chessclock_multiplayer/ui/ChckmViewModel.kt
@@ -0,0 +1,84 @@
+package alexman.chessclock_multiplayer.ui
+
+import alexman.chessclock_multiplayer.model.ClockSet
+import alexman.chessclock_multiplayer.model.Profile
+import alexman.chessclock_multiplayer.model.TimeControl
+import alexman.chessclock_multiplayer.repository.PersistentRepository
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+data class ListDataState<T>(
+    val data: List<T> = listOf(),
+)
+
+class ChckmViewModel(
+    private val profileRepository: PersistentRepository<Profile>,
+    private val timeControlRepository: PersistentRepository<TimeControl>,
+    private val clockSetRepository: PersistentRepository<ClockSet>,
+): ViewModel() {
+
+    private val _profileListDataUiState = MutableStateFlow(ListDataState<Profile>())
+    val profileListDataUiState = _profileListDataUiState.asStateFlow()
+
+    private val _timeControlListDataUiState = MutableStateFlow(ListDataState<TimeControl>())
+    val timeControlListDataUiState = _timeControlListDataUiState.asStateFlow()
+
+    private val _clockSetListDataUiState = MutableStateFlow(ListDataState<ClockSet>())
+    val clockSetListDataUiState = _clockSetListDataUiState.asStateFlow()
+
+    init {
+        refreshProfileState()
+        refreshTimeControlState()
+        refreshClockSetState()
+    }
+
+    fun writeProfile(item: Profile) {
+        profileRepository.writeItem(item)
+        refreshProfileState()
+    }
+
+    fun deleteProfile(item: Profile) {
+        profileRepository.deleteItem(item.id)
+        refreshProfileState()
+    }
+
+    fun writeTimeControl(item: TimeControl) {
+        timeControlRepository.writeItem(item)
+        refreshTimeControlState()
+    }
+
+    fun deleteTimeControl(item: TimeControl) {
+        timeControlRepository.deleteItem(item.id)
+        refreshTimeControlState()
+    }
+
+    fun writeClockSet(item: ClockSet) {
+        clockSetRepository.writeItem(item)
+        refreshClockSetState()
+    }
+
+    fun deleteClockSet(item: ClockSet) {
+        clockSetRepository.deleteItem(item.id)
+        refreshClockSetState()
+    }
+
+    private fun refreshProfileState() {
+        _profileListDataUiState.update { currentData ->
+            currentData.copy(data = profileRepository.readAllItems())
+        }
+    }
+
+    private fun refreshTimeControlState() {
+        _timeControlListDataUiState.update { currentData ->
+            currentData.copy(data = timeControlRepository.readAllItems())
+        }
+    }
+
+    private fun refreshClockSetState() {
+        _clockSetListDataUiState.update { currentData ->
+            currentData.copy(data = clockSetRepository.readAllItems())
+        }
+    }
+}
