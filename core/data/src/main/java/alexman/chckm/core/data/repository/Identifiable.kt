976296bab4efbdf6f84ca98d9e1732c24216d2a2
Alex Mandelias <alexmandelias@gmail.com>
2024-08-21 22:35:59 +0300

    Movies Idenfitiable to its own file

---
+++ core/data/src/main/java/alexman/chckm/core/data/repository/Identifiable.kt
@@ -0,0 +1,13 @@
+package alexman.chckm.core.data.repository
+
+// TODO: document
+interface Identifiable {
+    var id: Int
+    // this goes against existing naming conventions
+    // however, this val functions as a companion object
+    // constant, it should not be treated as a member,
+    // and the different name indicates exactly that
+    // (it's not possible to declare an abstract and
+    // overridable companion object constant in an Interface)
+    val id_not_set_constant: Int
+}
