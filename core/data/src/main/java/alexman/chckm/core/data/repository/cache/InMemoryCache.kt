49e0388ba76468a4a504cc40447a9225a2911157
Alex Mandelias <alexmandelias@gmail.com>
2024-08-30 19:28:19 +0300

    Cleans up InMemoryCache implementation

--- core/data/src/main/java/alexman/chckm/core/data/repository/cache/InMemoryCache.kt
+++ core/data/src/main/java/alexman/chckm/core/data/repository/cache/InMemoryCache.kt
@@ -8,38 +8,38 @@ class InMemoryCache<T : Identifiable> : Cache<T> {
     private val cache = mutableMapOf<Int, T>()
 
     override fun readItem(id: Int): T {
-        ensureIdExists(id)
+        requireIdInCache(id)
         return cache[id]!!
     }
 
-    override fun readAllItems(): List<T> {
-        return cache.values.toList()
-    }
+    override fun readAllItems(): List<T> = cache.values.toList()
 
     override fun writeItem(item: T) {
-        // ID_NOT_SET indicates creation of a new item
-        // otherwise indicates update of an existing one
-        val newId = if (item.id == item.id_not_set_constant)
-            nextAvailableId(item) else item.id
-
-        // don't change the original item's id
-        val newItem = item.copySetId(newId)
+        // an item with id `id_not_set_constant` indicates creation of a new item,
+        // otherwise indicates update of an existing one with that id.
+        // when creating a new item, we need to assign an id to it
+        val newItem = item.takeIf { it.id != item.id_not_set_constant }
+            ?: item.copySetId(newId = nextAvailableId(item))
 
         // safe cast, since copySetId() should return
         // an Identifiable of the same class as item
-        cache[newItem.id] = newItem as T
+        cache.writeIdentifiable(newItem as T)
     }
 
     override fun deleteItem(id: Int) {
-        ensureIdExists(id)
+        requireIdInCache(id)
         cache.remove(id)
     }
 
     private fun nextAvailableId(item: T): Int =
         (cache.keys.maxOrNull() ?: item.id_not_set_constant) + 1
 
-    private fun ensureIdExists(id: Int) {
-        if (id !in cache)
-            throw IllegalArgumentException("Item with id $id not found")
+    private fun requireIdInCache(id: Int) =
+        require(id in cache) {
+            "Item with id $id not found"
+        }
+
+    private fun MutableMap<Int, T>.writeIdentifiable(item: T) {
+        this[item.id] = item
     }
 }
